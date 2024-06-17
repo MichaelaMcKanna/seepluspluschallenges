5f099183d70bf9ad605b58ed3a4538faf2a1ca30
Michaela McKanna <78305965+MichaelaMcKanna@users.noreply.github.com>
2024-06-17 17:06:58 +0000

    complete challenge 2

--- src/02/Challenge/ch2_sort.cpp
+++ src/02/Challenge/ch2_sort.cpp
@@ -16,9 +16,29 @@
 // Returns: A boolean value: True on success, false otherwise.
 bool sort_array(int *arr, int n){
 
-    // Write your code here
-
+if(arr == nullptr || n<=0) { 
     return false;
+}
+
+bool swapped;
+
+for (int i = 0; i < n - 1; i++){
+     for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+          if (!swapped) {
+            break;
+        }
+}
+
+return true;
+    
 }
 
 // Main function
